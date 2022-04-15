c8494fdc995265b4bb0c2bd34dbf3f8c05341e95
Tolmeros@github.com <test>
2022-04-15 18:29:43 +0300

    Added koa static

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const Koa = require('koa');
+const serve = require('koa-static');
+const app = new Koa();
+
+app.use(serve('static/'));
+
+/*
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+*/
+
+app.listen(3000);
